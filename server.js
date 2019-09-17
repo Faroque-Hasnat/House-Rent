1db80d26e86c089c981845639a8c67b5a6f97fff
Faroque-Hasnat <42097019+Faroque-Hasnat@users.noreply.github.com>
2019-09-17 15:42:14 +0600

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/api/transactions', transactionRoute)
 app.use('/api/users', userRoute)
 
 if(process.env.NODE_ENV === 'production') {
-   app.user(express.static('client/build'))
+   app.use(express.static('client/build'))
    app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', index.html))
    })
